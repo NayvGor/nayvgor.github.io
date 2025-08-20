5496171198c9d012b3975a24acee16ba22dbe275
NayvGor <107840210+NayvGor@users.noreply.github.com>
2025-08-20 21:11:11 +0200

    Add files via upload

---
+++ js/footer.js
@@ -0,0 +1,16 @@
+footer.innerHTML = `
+<footer>
+    <nav class="extern-nav">
+        <ul>
+            <a href="https://x.com/NayvGor" target="_blank">
+                <li>X</li>
+            </a>
+            <a href="https://lospec.com/nayvgor" target="_blank">
+                <li>lospec</li>
+            </a>
+            <a href="https://www.youtube.com/@NayvGor" target="_blank">
+                <li>YouTube</li>
+            </a>
+        </ul>
+    </nav>
+</footer>`
---
+++ js/header.js
@@ -0,0 +1,14 @@
+header.innerHTML = `
+<header>
+    <nav class="intern-nav">
+        <ul>
+            <a href="about.html">
+                <li>About</li>
+            </a>
+            <a href="index.html">
+                <li>Work</li>
+            </a>
+        </ul>
+    </nav>
+</header>
+`;
