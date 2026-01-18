b99e6355b4357cff6558f9ff18a4376ae5384bc3
NayvGor <107840210+NayvGor@users.noreply.github.com>
2026-01-18 22:35:17 +0100

    Update header.js

--- js/header.js
+++ js/header.js
@@ -2,13 +2,11 @@ header.innerHTML = `
 <header>
     <nav class="intern-nav">
         <ul>
-            <a href="about.html">
-                <li>About</li>
-            </a>
             <a href="index.html">
                 <li>Work</li>
             </a>
         </ul>
     </nav>
 </header>
+
 `;
